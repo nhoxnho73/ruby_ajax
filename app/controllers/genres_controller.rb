3c716022984c70dcf9d1900feaa8b09173a7f967
phonglt <phonglt@ominext.com>
2020-07-28 16:50:44 +0700

    group name in update movie_list

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -15,7 +15,7 @@ class GenresController < ApplicationController
   def update_movie_list
     
     if params[:genre_id].present?
-      @movies = Movie.where(genre_id: params[:genre_id]).all
+      @movies = Movie.where(genre_id: params[:genre_id]).group(:name)
     end
       
     render json: { movies:  @movies }
@@ -24,7 +24,7 @@ class GenresController < ApplicationController
   def search_movie_lists
     if params[:list_search].present? 
       @searchs = Movie.where("name IN (?) OR release_date IN (?) OR director IN (?) OR summary LIKE ?", params[:list_movie], params[:list_year], params[:list_keyword], "%#{params[:list_search]}%").uniq
-    else
+    elsif  params[:list_movie].present? || params[:list_year].present? || params[:list_keyword].present
       @searchs = Movie.where("name IN (?) OR release_date IN (?) OR director IN (?)", params[:list_movie], params[:list_year], params[:list_keyword]).uniq
     end
     
